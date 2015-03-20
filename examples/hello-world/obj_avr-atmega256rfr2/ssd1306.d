d00738b908b6b60d76f733ba22d22795dee474f6
root <kanchandebnath@gmail.com>
2015-03-20 06:49:58 +0100

    try

---
+++ examples/hello-world/obj_avr-atmega256rfr2/ssd1306.d
@@ -0,0 +1,13 @@
+obj_avr-atmega256rfr2/ssd1306.o: \
+ ../../platform/avr-atmega256rfr2/dev/ssd1306.c \
+ ../../platform/avr-atmega256rfr2/dev/ssd1306.h ../../core/sys/clock.h \
+ ../../platform/avr-atmega256rfr2/contiki-conf.h \
+ ../../platform/avr-atmega256rfr2/platform-common.h \
+ ../../platform/avr-atmega256rfr2/platform-common.h \
+ ../../platform/avr-atmega256rfr2/dev/font.h
+ ../../platform/avr-atmega256rfr2/dev/ssd1306.c :
+ ../../platform/avr-atmega256rfr2/dev/ssd1306.h ../../core/sys/clock.h :
+ ../../platform/avr-atmega256rfr2/contiki-conf.h :
+ ../../platform/avr-atmega256rfr2/platform-common.h :
+ ../../platform/avr-atmega256rfr2/platform-common.h :
+ ../../platform/avr-atmega256rfr2/dev/font.h :
